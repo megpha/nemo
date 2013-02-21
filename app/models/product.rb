f62f545408a261d41db8e2cd24d7a9d7f5fb7d91
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2013-02-21 15:33:27 -0500

    remove pods from git

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product
   attr_accessor :remote_id, :name, :info, :cost, :image, :images
+
+  def primary_image
+    UIImage.alloc.initWithData(NSData.dataWithContentsOfURL(NSURL.URLWithString(image)))
+  end
 end
