dd29d4bfb59fb6906942e1c755254e73a23bafa0
Subba Rao Pasupuleti <subbarao.pasupuleti@gmail.com>
2013-02-21 16:13:02 -0500

    removing this thing

--- app/controllers/show_product_view_controller.rb
+++ app/controllers/show_product_view_controller.rb
@@ -2,13 +2,15 @@ class ShowProductViewController < UITableViewController
   extend IB
 
   outlet :name, UILabel
+  outlet :info, UILabel
+  outlet :price, UILabel
   outlet :picture, UIImageView
   attr_accessor :product, :image_index
 
   def viewDidLoad
     self.name.text  = product.name
-    #self.info.text  = product.info
-    #self.price.text  = product.cost
+    self.info.text  = product.info
+    self.price.text  = product.cost
     self.image_index = 0
 
     refresh_image
